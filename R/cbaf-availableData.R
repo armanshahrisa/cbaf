2d99dc3bea5f91c6166a42b36cb0dd82d15d78c5
armanshahrisa <shahrisa.arman@hotmail.com>
2017-06-26 00:46:30 +0430

    - Working availableDta() - Complete package

--- R/cbaf-availableData.R
+++ R/cbaf-availableData.R
@@ -23,7 +23,7 @@
 #' global enviroment (user's workspace) as 'available.data.types.output'. For convenience, an
 #' excel file - 'Available Data Types Output.xlsx' - is also generated in the working directory.
 #'
-#' @usage available.data.types()
+#' @usage availableData()
 #'
 #' @author Arman Shahrisa, \email{shahrisa.arman@hotmail.com} [maintainer, copyright holder]
 #' @author Maryam Tahmasebi Birgani, \email{tahmasebi-ma@ajums.ac.ir}
@@ -120,12 +120,12 @@ availableData <- function(outputName, excelFile = TRUE){
 
 
 
-  print("Cheching which data types are available for each cancer")
+  print("Cheching the available data for every cancer study")
 
 
   # create progress bar
 
-  pb <- txtProgressBar(min = 0, max = length(all.cancers), style = 3)
+  pb <- txtProgressBar(min = 0, max = nrow(list.of.studies), style = 3)
 
   i <- 0
 
@@ -136,7 +136,7 @@ availableData <- function(outputName, excelFile = TRUE){
 
   # looking for supported technique data
 
-  list.of.available.data <- sapply(studies[, "cancer_study_id"], function(cs, cgds) {
+  list.of.available.data <- sapply(list.of.studies[, "cancer_study_id"], function(cs, cgds) {
 
     description <- getCaseLists(cgds, cs)[, "case_list_name"]
 
@@ -144,13 +144,13 @@ availableData <- function(outputName, excelFile = TRUE){
 
     setTxtProgressBar(pb, i)
 
-    c(RNAseq = any(RNA_seq.terms %in% description),
+    c(RNA.seq = any(RNA_seq.terms %in% description),
 
-      microRNA = any(microRNA_seq.terms %in% description),
+      microRNA.seq = any(microRNA_seq.terms %in% description),
 
-      microarray_mRNA = any(microarray.for.mRNA.term %in% description),
+      microarray_of_mRNA = any(microarray.for.mRNA.term %in% description),
 
-      microarray_miRNA = any(microarray.for.miRNA.term %in% description),
+      microarray_of_miRNA = any(microarray.for.miRNA.term %in% description),
 
       methylation = any(methylation.term %in% description))
 
@@ -177,6 +177,12 @@ availableData <- function(outputName, excelFile = TRUE){
 
   combined.list <- cbind(list.of.studies[,"cancer_study_id"], list.of.studies[,"name"], t(list.of.available.data), list.of.studies[,"description"])
 
+  colnames(combined.list) <- c("cancer_study_id", "cancer_study_name", "RNA.seq", "microRNA.seq", "microarray_of_mRNA",
+
+                               "microarray_of_miRNA", "methylation", "description")
+
+  rownames(combined.list) <- 1:nrow(combined.list)
+
 
 
   ################################################
@@ -190,215 +196,18 @@ availableData <- function(outputName, excelFile = TRUE){
 
   # Converting matrix to data.frame
 
-  available.data.dataframe <- data.frame(available.data.matrix)
+  if(excelFile == TRUE){
 
-  colnames(available.data.dataframe) <- colnames(available.data.matrix)
+    combined.list.dataframe <- data.frame(combined.list)
 
-  write.xlsx(available.data.dataframe, file="Available Data Types Output.xlsx")
+    colnames(combined.list.dataframe) <- gsub("_", " ", colnames(combined.list.dataframe))
 
-  print(paste("An excel file entitled 'Available Data Types Output.xlsx' ,which shows the availabile data types for each cancer study, was saved in",
+    colnames(combined.list.dataframe) <- gsub("\\.", "-", colnames(combined.list.dataframe))
 
-              getwd(), ".", "In adition, the output is available in the global enviroment as 'available.data.types.output'", sep=" "))
+    rownames(combined.list.dataframe) <- 1:nrow(combined.list)
+
+    write.xlsx(combined.list.dataframe, file=paste(outputName, ".xlsx", sep = ""))
+
+  }
 
 }
-
-
-
-
-
-
-
-
-
-
-# The function has three parts -- find all studies, get case lists, output
-# to Excel. I revised it into three separate functions, which might be
-# documented on the same page. The first gets all studies
-
-available.studies <- function() {
-  mycgds <- cgdsr::CGDS("http://www.cbioportal.org/")
-  getCancerStudies(mycgds)
-}
-
-# I revised available.data.types as
-
-available.data.types <- function(studies = available.studies()) {
-  ## relevant descriptions
-  RNASeq <- c(
-    "Tumor Samples with mRNA data (RNA Seq V2)",
-    "Tumors with mRNA data (RNA Seq V2)",
-    "Tumor Samples with mRNA data (RNA Seq)",
-    "Tumors with mRNA data (RNA Seq)"
-  )
-  microRNA <- "Tumors with microRNA data (microRNA-Seq)"
-  mRNA <- c(
-    "Tumor Samples with mRNA data (Agilent microarray)",
-    "Tumors with mRNA data (Agilent microarray)",
-    "Tumor Samples with mRNA data (U133 microarray only)",
-    "Tumors with mRNA data"
-  )
-  miRNA <- "Tumors with microRNA"
-  methylation <- c(
-    "Tumor Samples with methylation data (HM450)",
-    "Tumors with methylation data (HM450)",
-    "Tumor Samples with methylation data (HM27)",
-    "Tumors with methylation data (HM27)",
-    "Tumors with methylation data"
-  )
-
-  ## query each study
-  mycgds <- CGDS("http://www.cbioportal.org/")
-  pb <- txtProgressBar(min = 0, max = nrow(studies), style = 3)
-  i <- 0
-  available <- sapply(studies[, "cancer_study_id"], function(se, cgds) {
-    description <- getCaseLists(cgds, se)[, "case_list_name"]
-    i <<- i + 1
-    setTxtProgressBar(pb, i)
-    c(RNASeq = any(description %in% RNASeq),
-      microRNA = any(description %in% microRNA),
-      microarray_mRNA = any(description %in% mRNA),
-      microarray_miRNA = any(description %in% miRNA),
-      methylation = any(description %in% methylation))
-  }, mycgds)
-  close(pb)
-
-
-  cbind(studies, t(available))
-}
-
-
-getCaseLists(mycgds,data.list[se,1])[,2]
-
-# The main changes are
-
-# 1. provide an argument with default value, preserving original behavior
-
-# 2. collect constant strings to an 'initialization' section of the function
-
-# 3. use sapply() to manage memory, rather than allocating and filling a
-# matrix
-
-# 4. respect R's notion of data types, e.g., logical() values rather than
-# "TRUE" / "FALSE" character strings; using a data.frame (because columns
-# are of different type) rather than matrix.
-
-#5. avoid querying the web service for getCancerStudies() on each iteration
-
-#6. not printing the result to an Excel file
-
-#7. returning the result to the user for subsequent processing, without
-#writing to the global environment.
-
-#I created a third function to write to Excel. It expects a file name
-#from the user and will not overwrite an existing file. It returns the
-#file name where the output can be found
-
-write.xls <- function(data.types, file) {
-stopifnot(!file.exists(file))
-## your implementation here
-file
-}
-
-
-# On the help page I might illustrate this as
-
-# studies <- available.studies()
-# head(studies)
-
-# then use a subset of the data for the next function
-
-# data.types <- available.data.types(head(studies))
-# data.types
-
-# and finally write the output to a temporary file
-
-xls.file <- write.xls(data.types, tempfile(fileext=".xls"))
-xls.file    # open in Excel
-
-# You might wish to write a final function that integrates these steps
-
-all.available.data.types <- function(file) {
-stopifnot(!file.exists(file))
-studies = available.studies()
-data.types = available.data.types(studies)
-write.xls(data.types, file)
-}
-
-# and include this as Michael suggests
-
-# \dontrun{
-# all.available.data.types("my.xls")
-# }
-
-#Bioconductor style would strongly discourage '.'-separated function
-# names, instead preferring camelCase() or snake_case().'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-genesList <- list(b=c("HOTAIR", "GAS5", "SNHG6"), hhc = c("PVT1", "GAS5", "DBBBGTZZXCC", "NSD2"))
-
-genesList <- list(b=c("HOTAIR", "GAS5"))
-
-cancers <- c("Acute Myeloid Leukemia (TCGA, NEJM 2013)", "Acute Myeloid Leukemia (TCGA, Provisional)")
-
-
-
-# obtainMultipleStudies(genesList, "test", cancers, desiredTechnique = "RNA-seq")
-
-
-
-
-
-
-
-
-# obtainOneStudy(lysine1, "test2", "Breast Invasive Carcinoma (TCGA, Cell 2015)", "RNA-seq", desiredCaseList = c(2,3,4,5), validateGenes = TRUE)
-
-
-# automatedStatistics(obtainedDataType = "single study", "test2", "RNA-seq", calculate = c("frequencyPercentage", "frequencyRatio", "meanValue", "medianValue"), cutoff=NULL, round=TRUE, topGenes = TRUE)
-
-
-
-
-
-
-
-
-# processMultipleStudies(genesList, "test2", cancers, "RNA-seq", cutoff = 2)
-
-# processOneStudy(lysine1, "test2", cancername, "RNA-seq", c(2,3,4,5))
-
-
-
-
-
-# Creating Empty matrix in order to fill with availibility data
-
-#available.data.matrix <- matrix(rep(" ", length(all.cancers)), nrow = length(all.cancers), ncol = 6)
-
-#rownames(available.data.matrix) <- 1:length(all.cancers)
-
-#available.data.matrix[,1] <- all.cancers
-
-#colnames(available.data.matrix) <- c("Cancers Study" ,"RNA-seq", "MicroRNA-seq", "Microarray (mRNA)", "Microarray (miRNA)", "Methylation")
-
-
